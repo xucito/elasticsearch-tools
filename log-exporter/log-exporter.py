a452c7a0b2570ab6e5ffdcef294e75a928dc760f
Tony Nguyen <tnguyen@etisalat.ae>
2020-04-10 22:40:29 +0400

    Add log exporter script

---
+++ log-exporter/log-exporter.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import ssl
+from elasticsearch.connection import create_ssl_context
+from elasticsearch.helpers import scan
+
+username = ''
+password = ''
+url = ''  # url or ip-address of the node
+port = 9200
+scheme = 'https'
+index = 'logs'
+output = 'output.csv'
+
+query = {
+    "query": {
+        "match_all": {}
+    },
+    "sort": [
+        {
+            "@timestamp": {
+                "order": "desc"
+            }
+        }
+    ]
+}
+
+ssl_context = create_ssl_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+es = Elasticsearch([url],
+                   http_auth=(username, password),
+                   scheme=scheme,
+                   port=port,
+                   ssl_context=ssl_context,
+                   timeout=30000)
+res = scan(es,
+           query=query,
+           index=index,
+           size=10000,
+           request_timeout=30000)
+
+file1 = open(output, "w")
+
+finalString = ""
+logsWritten = 1
+failedMessages = []
+for item in res:
+    try:
+        file1.write(item["_source"]["host"] + "," + item["_source"]
+                    ["@timestamp"] + ","+item["_source"]["message"] + "\n")
+    except:
+        print("An exception occurred on item " + item["_source"]["message"])
+        failedMessages.append(item["_source"]["message"])
+print("ALL FAILED MESSAGES")
+for item in failedMessages:
+    print(item)
